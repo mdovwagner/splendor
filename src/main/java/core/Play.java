edfd5185b877f375af421c0c621935516c726221
Michael Wagner <michaelwagner@Michaels-Air-3.home>
2017-12-30 20:59:59 -0500

    Concurrent gameplay

--- src/main/java/core/Play.java
+++ src/main/java/core/Play.java
@@ -1,10 +1,21 @@
 package core;
 
+import java.net.ServerSocket;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Play {
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(16);
+//    private static final List<Integer> results = new CopyOnWriteArrayList<>();
+    private static final Map<Network,Integer> results = new ConcurrentHashMap<>();
 
 
 
-    private static int runGame(Network network) {
+    private static int runOneGame(Network network) {
         Game g = new Game(network,"Michael");
         while (!g.isGameOver()) {
             g = g.pickNextMove();
@@ -12,13 +23,18 @@ public class Play {
         return g.getTurn();
     }
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
         // First generation
+//        ServerSocket socket = new ServerSocket(8000);
         for (int i = 0; i < 100; i++) {
-            Network network = new Network();
-            System.out.println(runGame(network));
+            threadPool.submit(()-> {
+                Network network = new Network();
+                results.put(network,runOneGame(network));
+            });
         }
+        while (results.size() < 100) {} // spins until everything is done;
+        System.out.println(results.values().toString());
+        System.exit(0);
 
 
 
